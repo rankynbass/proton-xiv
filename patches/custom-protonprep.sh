e5d4ba1a1111b6552fe3fc5c8d80b687a5418d94
Rankyn Bass <rankyn@proton.me>
2024-01-24 17:25:14 -0800

    Fix path error

--- patches/custom-protonprep.sh
+++ patches/custom-protonprep.sh
@@ -17,6 +17,6 @@ patch -Np1 < ../patches/custom/dualsense1.patch
 patch -Np1 < ../patches/custom/dualsense2.patch
 
 echo "WINE: Add Wine-to-Unix PID mapping"
-patch -Np1 < ../paches/custom/proton8-unix-pid-maps.patch
+patch -Np1 < ../patches/custom/proton8-unix-pid-maps.patch
 
 popd
