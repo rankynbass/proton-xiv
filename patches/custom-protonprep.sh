e811c3cba3e4c613b069d9d8faa231c67bfd09cc
Rankyn Bass <rankyn@proton.me>
2024-01-24 17:22:15 -0800

    Fix custom-protonprep

--- patches/custom-protonprep.sh
+++ patches/custom-protonprep.sh
@@ -5,18 +5,18 @@ git reset --hard HEAD
 git clean -xdf
 git checkout -b xiv-temp
 git revert --no-commit df43fd788455ed243af61ffb76102ab1ef6557e2
-git commit -a "Revert problematic commit df43fd7"
+git commit -a -m "Revert problematic commit df43fd7"
 popd
 
 ./patches/protonprep-valve-staging.sh
 
 pushd wine
 
-echo "WINE: -DualSense- Add PS5 Controller support
+echo "WINE: -DualSense- Add PS5 Controller support"
 patch -Np1 < ../patches/custom/dualsense1.patch
 patch -Np1 < ../patches/custom/dualsense2.patch
 
-echo "WINE: -XIVLauncher.Core- Add Wine-to-Unix PID mapping"
+echo "WINE: Add Wine-to-Unix PID mapping"
 patch -Np1 < ../paches/custom/proton8-unix-pid-maps.patch
 
 popd
