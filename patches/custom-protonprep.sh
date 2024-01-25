0df54b1753ba19b3292e6b5eefcc56ed0cb77509
Rankyn Bass <rankyn@proton.me>
2024-01-24 17:08:44 -0800

    Add XIV-Proton patches

---
+++ patches/custom-protonprep.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+pushd wine
+git reset --hard HEAD
+git clean -xdf
+git checkout -b xiv-temp
+git revert --no-commit df43fd788455ed243af61ffb76102ab1ef6557e2
+git commit -a "Revert problematic commit df43fd7"
+popd
+
+./patches/protonprep-valve-staging.sh
+
+pushd wine
+
+echo "WINE: -DualSense- Add PS5 Controller support
+patch -Np1 < ../patches/custom/dualsense1.patch
+patch -Np1 < ../patches/custom/dualsense2.patch
+
+echo "WINE: -XIVLauncher.Core- Add Wine-to-Unix PID mapping"
+patch -Np1 < ../paches/custom/proton8-unix-pid-maps.patch
+
+popd
