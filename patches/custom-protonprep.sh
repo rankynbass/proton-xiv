9ceb9b167a8a64c29bbf694c2f8bbef64f9b30d7
Rankyn Bass <rankyn@proton.me>
2024-01-28 15:31:37 -0800

    Add ping fix

--- patches/custom-protonprep.sh
+++ patches/custom-protonprep.sh
@@ -20,4 +20,7 @@ patch -Np1 < ../patches/custom/dualsense2.patch
 echo "WINE: Add Wine-to-Unix PID mapping"
 patch -Np1 < ../patches/custom/proton8-unix-pid-maps.patch
 
+echo "WINE: Add Ping Fix"
+patch -Np1 < ../patches/custom/ping-fix.patch
+
 popd
