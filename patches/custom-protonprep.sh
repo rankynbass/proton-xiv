efac477238fec6b82c4644b7f24cf3e09272b5e2
Rankyn Bass <rankyn@proton.me>
2024-03-20 13:22:23 -0700

    Add hide wine exports ..testing

--- patches/custom-protonprep.sh
+++ patches/custom-protonprep.sh
@@ -23,4 +23,7 @@ patch -Np1 < ../patches/custom/proton9-unix-pid-maps.patch
 echo "WINE: Add Ping Fix"
 patch -Np1 < ../patches/custom/ping-fix.patch
 
+echo "WINE: Add Hide Wine Exports"
+patch -Np1 < ../patches/custom/add-hide-wine-exports-to-mainline.patch
+
 popd
