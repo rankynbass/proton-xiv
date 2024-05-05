d24be58aca045052376921a0f1acadc57974f0a5
Rankyn Bass <rankyn@proton.me>
2024-05-05 14:19:23 -0700

    Restore proton8-unix-pid-maps.patch

--- patches/custom-protonprep.sh
+++ patches/custom-protonprep.sh
@@ -7,7 +7,7 @@ patch -Np1 < ../patches/custom/dualsense1.patch
 patch -Np1 < ../patches/custom/dualsense2.patch
 
 echo "WINE: Add Wine-to-Unix PID mapping"
-patch -Np1 < ../patches/custom/proton9-unix-pid-maps.patch
+patch -Np1 < ../patches/custom/proton8-unix-pid-maps.patch
 
 echo "WINE: Add Ping Fix"
 patch -Np1 < ../patches/custom/ping-fix.patch
