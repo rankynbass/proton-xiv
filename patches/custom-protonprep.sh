a21a89bb99664a4bace1a1a341cdbedd433c5e6c
Rankyn Bass <rankyn@proton.me>
2024-08-31 20:12:41 -0700

    Add XIV patches to GE-Proton

---
+++ patches/custom-protonprep.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+./patches/protonprep-valve-staging.sh
+
+pushd wine
+
+echo "WINE: Add Wine-to-Unix PID mapping"
+patch -Np1 < ../patches/custom/proton9-unix-pid-maps.patch
+
+echo "WINE: Add Ping Fix"
+patch -Np1 < ../patches/custom/ping-fix.patch
+
+popd
