07f72a99cc3e635b49e95018e90170633b3154aa
Rankyn Bass <rankyn@proton.me>
2024-09-29 15:37:37 -0700

    Remove broken dualsense patches

--- patches/custom-protonprep.sh
+++ patches/custom-protonprep.sh
@@ -8,8 +8,4 @@ patch -Np1 < ../patches/custom/proton9-unix-pid-maps.patch
 echo "WINE: Add Ping Fix"
 patch -Np1 < ../patches/custom/ping-fix.patch
 
-echo "WINE: Add DualSense patches"
-patch -Np1 < ../patches/custom/dualsense1.patch
-patch -Np1 < ../patches/custom/dualsense2.patch
-
 popd
