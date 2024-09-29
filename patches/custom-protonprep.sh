6cf7de407d3de6524d21a214f199b92e127ee8a6
Rankyn Bass <rankyn@proton.me>
2024-09-29 13:13:16 -0700

    Re-add dualsense patches

--- patches/custom-protonprep.sh
+++ patches/custom-protonprep.sh
@@ -10,4 +10,8 @@ patch -Np1 < ../patches/custom/proton9-unix-pid-maps.patch
 echo "WINE: Add Ping Fix"
 patch -Np1 < ../patches/custom/ping-fix.patch
 
+echo "WINE: Add DualSense patches"
+patch -Np1 < ../patches/custom/dualsense1.patch
+patch -Np1 < ../patches/custom/dualsense2.patch
+
 popd
