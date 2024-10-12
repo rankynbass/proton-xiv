52dcb1ca1876f41e74a2f753a92f40be774b4329
Rankyn Bass <rankyn@proton.me>
2024-10-12 11:51:33 -0700

    Re-add dualsense patches. I guess they do something after all.

--- patches/custom-protonprep.sh
+++ patches/custom-protonprep.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+./patches/protonprep-valve-staging.sh
+
 pushd wine
 
 echo "WINE: Add Wine-to-Unix PID mapping"
@@ -7,5 +9,9 @@ patch -Np1 < ../patches/custom/proton9-unix-pid-maps.patch
 
 echo "WINE: Add Ping Fix"
 patch -Np1 < ../patches/custom/ping-fix.patch
+
+echo "WINE: Add DualSense patches"
+patch -Np1 < ../patches/custom/dualsense1.patch
+patch -Np1 < ../patches/custom/dualsense2.patch
 
 popd
