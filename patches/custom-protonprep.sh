e82f72f1198043bece1da03e67e017477ec487b1
Rankyn Bass <rankyn@proton.me>
2024-12-13 16:42:27 -0800

    Update patchset

--- patches/custom-protonprep.sh
+++ patches/custom-protonprep.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-./patches/protonprep-valve-staging.sh
-
 pushd wine
 
 echo "WINE: Add Wine-to-Unix PID mapping"
@@ -9,9 +7,5 @@ patch -Np1 < ../patches/custom/proton9-unix-pid-maps.patch
 
 echo "WINE: Add Ping Fix"
 patch -Np1 < ../patches/custom/ping-fix.patch
-
-echo "WINE: Add DualSense patches"
-patch -Np1 < ../patches/custom/dualsense1.patch
-patch -Np1 < ../patches/custom/dualsense2.patch
 
 popd
