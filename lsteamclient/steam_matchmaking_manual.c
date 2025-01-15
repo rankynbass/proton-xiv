1a0811b6cca17af9ae5e064dd308cfc766d2810d
Rémi Bernon <rbernon@codeweavers.com>
2025-01-15 23:55:17 +0100

    lsteamclient: Manually wrap ISteamMatchmakingServers on the PE side.

--- lsteamclient/steam_matchmaking_manual.c
+++ lsteamclient/steam_matchmaking_manual.c
@@ -2,6 +2,106 @@
 
 WINE_DEFAULT_DEBUG_CHANNEL(steamclient);
 
+void * __thiscall winISteamMatchmakingServers_SteamMatchMakingServers002_RequestInternetServerList( struct w_iface *_this, uint32_t iApp, MatchMakingKeyValuePair_t **ppchFilters, uint32_t nFilters, w_ISteamMatchmakingServerListResponse_106 *pRequestServersResponse )
+{
+    struct ISteamMatchmakingServers_SteamMatchMakingServers002_RequestInternetServerList_params params =
+    {
+        .u_iface = _this->u_iface,
+        .iApp = iApp,
+        .ppchFilters = ppchFilters,
+        .nFilters = nFilters,
+        .pRequestServersResponse = pRequestServersResponse,
+    };
+
+    TRACE( "%p\n", _this );
+
+    STEAMCLIENT_CALL( ISteamMatchmakingServers_SteamMatchMakingServers002_RequestInternetServerList, &params );
+    return params._ret;
+}
+
+void * __thiscall winISteamMatchmakingServers_SteamMatchMakingServers002_RequestLANServerList( struct w_iface *_this, uint32_t iApp, w_ISteamMatchmakingServerListResponse_106 *pRequestServersResponse )
+{
+    struct ISteamMatchmakingServers_SteamMatchMakingServers002_RequestLANServerList_params params =
+    {
+        .u_iface = _this->u_iface,
+        .iApp = iApp,
+        .pRequestServersResponse = pRequestServersResponse,
+    };
+
+    TRACE( "%p\n", _this );
+
+    STEAMCLIENT_CALL( ISteamMatchmakingServers_SteamMatchMakingServers002_RequestLANServerList, &params );
+    return params._ret;
+}
+
+void * __thiscall winISteamMatchmakingServers_SteamMatchMakingServers002_RequestFriendsServerList( struct w_iface *_this, uint32_t iApp, MatchMakingKeyValuePair_t **ppchFilters, uint32_t nFilters, w_ISteamMatchmakingServerListResponse_106 *pRequestServersResponse )
+{
+    struct ISteamMatchmakingServers_SteamMatchMakingServers002_RequestFriendsServerList_params params =
+    {
+        .u_iface = _this->u_iface,
+        .iApp = iApp,
+        .ppchFilters = ppchFilters,
+        .nFilters = nFilters,
+        .pRequestServersResponse = pRequestServersResponse,
+    };
+
+    TRACE( "%p\n", _this );
+
+    STEAMCLIENT_CALL( ISteamMatchmakingServers_SteamMatchMakingServers002_RequestFriendsServerList, &params );
+    return params._ret;
+}
+
+void * __thiscall winISteamMatchmakingServers_SteamMatchMakingServers002_RequestFavoritesServerList( struct w_iface *_this, uint32_t iApp, MatchMakingKeyValuePair_t **ppchFilters, uint32_t nFilters, w_ISteamMatchmakingServerListResponse_106 *pRequestServersResponse )
+{
+    struct ISteamMatchmakingServers_SteamMatchMakingServers002_RequestFavoritesServerList_params params =
+    {
+        .u_iface = _this->u_iface,
+        .iApp = iApp,
+        .ppchFilters = ppchFilters,
+        .nFilters = nFilters,
+        .pRequestServersResponse = pRequestServersResponse,
+    };
+
+    TRACE( "%p\n", _this );
+
+    STEAMCLIENT_CALL( ISteamMatchmakingServers_SteamMatchMakingServers002_RequestFavoritesServerList, &params );
+    return params._ret;
+}
+
+void * __thiscall winISteamMatchmakingServers_SteamMatchMakingServers002_RequestHistoryServerList( struct w_iface *_this, uint32_t iApp, MatchMakingKeyValuePair_t **ppchFilters, uint32_t nFilters, w_ISteamMatchmakingServerListResponse_106 *pRequestServersResponse )
+{
+    struct ISteamMatchmakingServers_SteamMatchMakingServers002_RequestHistoryServerList_params params =
+    {
+        .u_iface = _this->u_iface,
+        .iApp = iApp,
+        .ppchFilters = ppchFilters,
+        .nFilters = nFilters,
+        .pRequestServersResponse = pRequestServersResponse,
+    };
+
+    TRACE( "%p\n", _this );
+
+    STEAMCLIENT_CALL( ISteamMatchmakingServers_SteamMatchMakingServers002_RequestHistoryServerList, &params );
+    return params._ret;
+}
+
+void * __thiscall winISteamMatchmakingServers_SteamMatchMakingServers002_RequestSpectatorServerList( struct w_iface *_this, uint32_t iApp, MatchMakingKeyValuePair_t **ppchFilters, uint32_t nFilters, w_ISteamMatchmakingServerListResponse_106 *pRequestServersResponse )
+{
+    struct ISteamMatchmakingServers_SteamMatchMakingServers002_RequestSpectatorServerList_params params =
+    {
+        .u_iface = _this->u_iface,
+        .iApp = iApp,
+        .ppchFilters = ppchFilters,
+        .nFilters = nFilters,
+        .pRequestServersResponse = pRequestServersResponse,
+    };
+
+    TRACE( "%p\n", _this );
+
+    STEAMCLIENT_CALL( ISteamMatchmakingServers_SteamMatchMakingServers002_RequestSpectatorServerList, &params );
+    return params._ret;
+}
+
 void __thiscall winISteamMatchmakingServers_SteamMatchMakingServers002_ReleaseRequest( struct w_iface *_this, void *hServerListRequest )
 {
     struct ISteamMatchmakingServers_SteamMatchMakingServers002_ReleaseRequest_params params =
