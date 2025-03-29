31064fc00a1d0d845b0eb0a76af8ef111dc2b05e
Rankyn Bass <rankyn@proton.me>
2025-03-28 19:48:22 -0700

    Add debug patch for XIVLauncher/Dalamud

--- patches/custom-protonprep.sh
+++ patches/custom-protonprep.sh
@@ -7,6 +7,9 @@ pushd wine
 echo "WINE: Add Wine-to-Unix PID mapping"
 patch -Np1 < ../patches/custom/proton9-unix-pid-maps.patch
 
+echo "WINE: Add winedbg patch to fix a Dalamud crash"
+patch -Np1 < ../patches/custom/dbghelp-ffxiv.patch
+
 echo "WINE: Add Ping Fix"
 patch -Np1 < ../patches/custom/ping-fix.patch
 
