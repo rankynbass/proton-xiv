49e5a1eb26fc9631cce75a777bc7dfcd2aaa86d7
Rankyn Bass <rankyn@proton.me>
2025-05-14 21:25:45 -0700

    Remove outdated patches

--- patches/add-ntsync.sh
+++ patches/add-ntsync.sh
@@ -3,6 +3,6 @@
 pushd wine
 
 echo "WINE: Add NTSync for Valve Bleeding Edge"
-patch -Np1 < ../patches/custom/ntsync-valvexbe.patch
+patch -Np1 < ../patches/custom/proton10-ntsync.patch
 
 popd
--- patches/custom-protonprep.sh
+++ patches/custom-protonprep.sh
@@ -5,23 +5,9 @@
 pushd wine
 
 echo "WINE: Add Wine-to-Unix PID mapping"
-patch -Np1 < ../patches/custom/proton9-unix-pid-maps.patch
+patch -Np1 < ../patches/custom/proton10-unix-pid-maps.patch
 
 echo "WINE: Add winedbg patch to fix a Dalamud crash"
 patch -Np1 < ../patches/custom/portable-pdb.patch
-
-echo "WINE: Add Ping Fix"
-patch -Np1 < ../patches/custom/ping-fix.patch
-
-echo "WINE: Add DualSense patches"
-patch -Np1 < ../patches/dualsense/0001-pending-review-mmdevapi-support-VT_CLSID-for-contain.patch
-patch -Np1 < ../patches/dualsense/0002-pending-review-mmdevapi-decode-ContainerId-property-.patch
-patch -Np1 < ../patches/dualsense/0003-pending-review-mmdevapi-copy-ContainerID-from-audio-.patch
-patch -Np1 < ../patches/dualsense/0004-pending-review-mmdevapi-Invalidate-ContainerID-of-un.patch
-patch -Np1 < ../patches/dualsense/0005-draft-winepulse-Store-PulseAudio-device-s-sysfs-path.patch
-patch -Np1 < ../patches/dualsense/0006-draft-winepulse-Add-support-for-containerId-property.patch
-patch -Np1 < ../patches/dualsense/0007-draft-winebus-store-container-sysfs-path-from-udev-b.patch
-patch -Np1 < ../patches/dualsense/0008-draft-winebus-implement-BusQueryContainerID-based-on.patch
-patch -Np1 < ../patches/dualsense/0009-WiP-Implement-SetupDiGetDeviceInterfacePropertyW-for.patch
 
 popd
