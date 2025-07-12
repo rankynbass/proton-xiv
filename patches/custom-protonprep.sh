4e49981b6aaf74e80aa8109fe95593301ed96b69
Rankyn Bass <rankyn@proton.me>
2025-07-11 19:42:19 -0700

    Remove debug patch (no longer needed)

--- patches/custom-protonprep.sh
+++ patches/custom-protonprep.sh
@@ -7,7 +7,4 @@ pushd wine
 echo "WINE: Add Wine-to-Unix PID mapping"
 patch -Np1 < ../patches/custom/proton10-unix-pid-maps.patch
 
-echo "WINE: Add winedbg patch to fix a Dalamud crash"
-patch -Np1 < ../patches/custom/portable-pdb.patch
-
 popd
